2d97bd7138008094502e3e77296e79a736d9556c
EvgenyKrupski <Krupev87@yandex.ru>
2022-05-21 14:14:00 +0300

    решение задачи 62

---
+++ 62/Program.cs
@@ -0,0 +1,51 @@
+﻿/*Заполните спирально массив 4 на 4.*/
+/*1     2   3   4
+
+12   13  14   5
+
+11   16  15   6
+
+10    9  8    7
+*/
+
+int a = 4;
+int [,] spiralArray = new int[a,a];
+int temp = 1;
+int i = 0;
+int j = 0;
+
+void PrintArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j]+ " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+while(temp<=a*a)
+{
+    spiralArray[i,j] = temp;
+    temp ++;
+    if (i<=j+1 && i+j<a-1)
+    {
+        j++;
+    }
+    else if(i<j && i+j>=a-1)
+    {
+        i++;
+    }
+    else if(i>=j && i+j>a-1)
+    {
+        j--;
+    }
+    else
+    {
+        i--;
+    }
+}
+
+PrintArray(spiralArray);
